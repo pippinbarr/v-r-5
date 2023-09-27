12955e7894f0e2b16ee04690db29a1f0e3dbd1a9
Pippin Barr <pippin.barr@gmail.com>
2023-09-26 22:18:58 -0400

    First draft of "lightswitches"
    - Part of having some substantial number of lights in this project is that it will probably overwhelm some system or some something if they're all on all the time (there's noticable lag if I even look at a building with a point light in it) - so I managed to get together basic scripts tied to triggers in my current testing room that will turn the light(s) inside off when you leave and on when you come in, meaning that essentially all lights can be off at all times unless you're in the room that uses them
    - I do like this because it's efficient but it also seems kind of physically reasonable - no need to cast shadows nobody can see, right?

---
+++ _unity-project/v-r-5/Assets/Scripts/TurnOnTheLights.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnOnTheLights : MonoBehaviour
+{
+    public Light[] Lights;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter() 
+    {
+        for (int i = 0; i < Lights.Length; i++)
+        {
+            Lights[i].GetComponent<Light>().enabled = true;
+        }
+    }
+}
